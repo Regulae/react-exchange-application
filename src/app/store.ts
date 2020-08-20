aacb8c9561c6ee3b47b099ada2dd473956793c0c
Regulae <engelhardt@noiseag.ch>
2020-08-20 10:13:30 +0200

    add rootReducer to combine reducers

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,14 +1,13 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import {configureStore, ThunkAction, Action} from '@reduxjs/toolkit';
+import {rootReducer} from './rootReducer';
 
 export const store = configureStore({
-  reducer: {
-  },
+    reducer: rootReducer
 });
 
 export type RootState = ReturnType<typeof store.getState>;
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
->;
+export type AppDispatch = typeof store.dispatch;
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType,
+    RootState,
+    unknown,
+    Action<string>>;
